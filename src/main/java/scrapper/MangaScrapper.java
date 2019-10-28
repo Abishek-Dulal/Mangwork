293dde8f51ea97f316a3a80941c0b8b6f42adc3c
abishek dulal <abishek.dulal666s@gmail.com>
2019-10-28 22:01:53 +0545

    Completed scrapped model

--- src/main/java/scrapper/MangaScrapper.java
+++ src/main/java/scrapper/MangaScrapper.java
@@ -2,15 +2,24 @@ package scrapper;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import scrapmodel.SiteMethods;
+import scrapmodel.query.QueryFactory;
+import scrapmodel.sites.SiteFactory;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Map;
 
 public class MangaScrapper  {
 
-      public Document scrapPage(String query) throws IOException {
+      private Document scrapPage(String query) throws IOException {
              Document doc  =Jsoup.connect(query).get();
              return doc;
+      }
+
+      public Map scrapPage(String site , Map properties, SiteMethods method) throws IOException {
+         PageProcessor processor = new PageProcessor();
+         Document document = scrapPage(QueryFactory.getSiteQuery(site).get().searchByAuthorOrBookname(properties));
+          return  processor.processPage(SiteFactory.getSite(site).get(),document,method);
       }
 
 
